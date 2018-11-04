e21d06bea9fd5b9c8c7d4146dc294a71da0a4bce
harshul-laiwala <harshul.laiwala@gmail.com>
2018-11-04 00:10:02 -0700

    Update 01-python/02-python/01-required/06-functions_intermediate_1.py

---
+++ 01-python/02-python/01-required/06-functions_intermediate_1.py
@@ -0,0 +1,11 @@
+# As part of this assignment, please create a function randInt() where
+
+import random
+
+# randInt() returns a random integer between 0 to 100
+def randInt():
+    
+
+# randInt(max=50) returns a random integer between 0 to 50
+# randInt(min=50) returns a random integer between 50 to 100
+# randInt(min=50, max=500) returns a random integer between 50 and 500
