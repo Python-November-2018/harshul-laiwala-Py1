f1eeee282a52fcfbf6acaca6de0993228128aff3
harshul-laiwala <harshul.laiwala@gmail.com>
2018-11-05 20:16:41 -0800

    Update 01-python/02-python/01-required/13-math_dojo.py

---
+++ 01-python/02-python/01-required/13-math_dojo.py
@@ -0,0 +1,28 @@
+# Assignment: MathDojo
+# Objectives:
+# Practice creating a class and creating new instances
+# Practice chaining methods
+# Practice writing flexible functions that can take a variable number of arguments
+# HINT: To do this exercise, you will probably have to use 'return self'. If the method returns itself (an instance of itself), we can chain methods.
+
+# Create a Python class called MathDojo that has the methods add and subtract. Have these 2 functions take at least 1 parameter.
+class MathDojo:
+    def __init__(self):
+        self.Result =0
+    
+    def add(self, *values ):
+        for value in values:
+            self.Result += value
+            return self 
+    def subtract(self, *values):
+        for value in values:
+            self.Result -= value
+            return self
+    def result(self):
+        return self.Result
+        
+# Then create a new instance called md. It should be able to do the following task:
+md=MathDojo()
+x=md.add(2).add(2,5,1).subtract(3,2).result()
+print(x) # should print 5
+# which should perform 0+2+(2+5+1)-(3+2) and print 5.
