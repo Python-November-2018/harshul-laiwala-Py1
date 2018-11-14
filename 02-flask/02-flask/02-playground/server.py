80429ef7d7cbad63f3158dbccd8cd64204c7369f
harshul-laiwala <harshul.laiwala@gmail.com>
2018-11-14 11:49:37 -0800

    playground

--- 02-flask/02-flask/02-playground/server.py
+++ 02-flask/02-flask/02-playground/server.py
@@ -2,5 +2,17 @@ from flask import Flask , render_template
 
 app = Flask(__name__)
 
-if __name__ = '__main__':
+@app.route("/play")
+def play():
+    return render_template('index.html' , num=3)
+
+@app.route("/play/<num>")
+def play_times(num):
+    return render_template('index.html', num=int(num))
+
+@app.route("/play/<num>/<color>")
+def play_times_color(num, color):
+    return render_template('index.html', num=int(num), color=color)
+
+if __name__ == '__main__':
     app.run(debug=True)
