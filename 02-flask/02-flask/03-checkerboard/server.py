ec65ac99002ace5958d018a3816afded0d95376c
harshul-laiwala <harshul.laiwala@gmail.com>
2018-11-16 01:37:01 -0800

    checkerboard

---
+++ 02-flask/02-flask/03-checkerboard/server.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def checkboard():
+    return render_template('index.html')
+
+
+@app.route('/<x>/<y>')
+def user_defined_checkerboard(x,y):
+    return render_template('index.html', x=int(x), y=int((int(y)/2)))
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
