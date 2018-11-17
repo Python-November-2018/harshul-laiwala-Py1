73ea10af7c212b32a4c8ab798b8a6f4cef6df645
harshul-laiwala <harshul.laiwala@gmail.com>
2018-11-17 04:34:47 -0800

    dojo survey

--- 02-flask/02-flask/05-dojo_survey/server.py
+++ 02-flask/02-flask/05-dojo_survey/server.py
@@ -5,13 +5,20 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return render_template('index.html')
+@app.route('/danger')
+def danger():
+    print('*'* 50 + ' a user tried to visit /danger.  we have redirected the user to "/" ' + '*'* 50)
+    return redirect('/')
 
 @app.route('/process', methods=['POST'])
 def process_survey_form():
-    print(request.form)
+    # print(request.form)
     survey_info = (
+        'name': request.form['name'],
+        
 
     )
+    print()
     return render_template('result.html')
 
 if __name__ == '__main__':
