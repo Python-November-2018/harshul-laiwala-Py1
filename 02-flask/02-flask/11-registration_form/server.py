889d4f3c756ab25747ee3460a16524d0b31cd327
harshul-laiwala <harshul.laiwala@gmail.com>
2018-11-27 03:36:29 -0800

    Update server.py

--- 02-flask/02-flask/11-registration_form/server.py
+++ 02-flask/02-flask/11-registration_form/server.py
@@ -15,20 +15,36 @@ def process_user_regisration():
     print(request.form)
     
     # All fields are required and must not be blank
-    for keys in request.form:
-        print('+' * 80)
-        print(keys)
-        for key in keys:
-            print('-'* 80)
-            print(key)
-            if request.form[key] == 0:
-                flash('Cannot be empty', key)
-                errors=True
+    if len(str(request.form['firstName'])) < 1:
+        errors=True
+        flash('First Name cannot be empty', category='error')
+    if len(str(request.form['lastName'])) < 1:
+        errors=True
+        flash('Last Name cannot be empty', category='error')
+    if len(str(request.form['email'])) < 1:
+        errors=True
+        flash('Email cannot be empty', category='error')
+    if len(str(request.form['password'])) < 1:
+        errors=True
+        flash('Password cannot be empty', category='error')
+    if len(str(request.form['confirmPassword'])) < 1:
+        errors=True
+        flash('Confirm Password cannot be empty', category='error')
+    
+    # for keys in request.form:
+    #     print('+' * 80)
+    #     print(keys)
+        # for key in keys:
+        #     print('-'* 80)
+        #     print(key)
+        # if request.form[keys] == 0:
+        #     flash('Cannot be empty', category= keys)
+        #     errors=True
     
     # First and Last Name cannot contain any numbers
     if bool(re.search(r'\d',str(request.form['firstName']))) or bool(re.search(r'\d', str(request.form['lastName']))):
         errors=True
-        flash('First and Last Name cannot contain any numbers',category='error')
+        flash('First and Last Name cannot contain any numbers', category='error')
 
     # Password should be more than 8 characters
     if len(str(request.form['password'])) <= 8:
@@ -38,7 +54,7 @@ def process_user_regisration():
     # Email should be a valid email
     if bool(re.search(r'^[a-zA-Z0-9*_*.]+@[a-zA-Z0-9]+.[a-zA-z0-9]{3}$',str(request.form['email']))) == False:
         errors=True
-        flash('Email should be a valid email' , category='error')
+        flash('Email should be a valid email', category='error')
 
     # Password and Password Confirmation should match
     if str(request.form['password']) != str(request.form['confirmPassword']):
@@ -49,7 +65,7 @@ def process_user_regisration():
         return redirect('/')
     
     flash('User Registered Successfully', category='success')
-    return redirect('/success.html')
+    return redirect('/')
     
 
     
