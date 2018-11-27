0476b70a8cd864a35f578e85bfe416d467fa675b
harshul-laiwala <harshul.laiwala@gmail.com>
2018-11-27 01:01:39 -0800

    Registration Form

--- 02-flask/02-flask/11-registration_form/server.py
+++ 02-flask/02-flask/11-registration_form/server.py
@@ -5,7 +5,7 @@ app.secret_key = 'this is a secret key'
 
 @app.route('/')
 def index():
-    return render_template(index.html)
+    return render_template('index.html')
 
 if __name__=='__main__':
     app.run(debug=True)
